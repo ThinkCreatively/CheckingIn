a938fa2ffdcfdc23c32afffaaf8d8cad63679f23
James Moore <jameswmoore99@gmail.com>
2024-02-05 10:57:13 -0700

    ~ Have article section wireframe set up

--- checking-in/app/customComps/articleSection/article.tsx
+++ checking-in/app/customComps/articleSection/article.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-const ArticleSection = () => {};
+const ArticleSection = () => {
+  return (
+    <div className="flex flex-col items-center border-2 border-yellow-900 w-full ">
+      <div className="text-3xl my-10">Todays Articles</div>
+      <div className="border-2 border-rose-500 w-full text-center">
+        Thinking a dropdown or something
+        <div>Topic 1 - AI</div>
+        <div>Topic 2 - Tech Jobs</div>
+        <div>Topic 3 - ETC</div>
+      </div>
+    </div>
+  );
+};
 
 export default ArticleSection;
--- checking-in/app/page.tsx
+++ checking-in/app/page.tsx
@@ -1,12 +1,11 @@
 import WeatherSection from "./customComps/weatherSection/weather";
+import ArticleSection from "./customComps/articleSection/article";
 
 export default function Home() {
   return (
     <main className="flex">
       <div className="flex h-screen w-full bg-gray-500 justify-evenly">
-        <div className="flex justify-center border-2 border-yellow-900 w-full ">
-          Section
-        </div>
+        <ArticleSection />
         <WeatherSection />
         <div className="flex justify-center border-2 border-yellow-900 w-full ">
           Section
