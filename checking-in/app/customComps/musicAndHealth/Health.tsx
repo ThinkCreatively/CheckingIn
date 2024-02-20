6fd05ad095d6296f9832c1024085ba615b46d4b1
James Moore <jameswmoore99@gmail.com>
2024-02-20 00:31:54 -0700

    ~ Finished basic wire frame for health

--- checking-in/app/customComps/musicAndHealth/Health.tsx
+++ checking-in/app/customComps/musicAndHealth/Health.tsx
@@ -3,18 +3,21 @@ import React from "react";
 const Health = () => {
   const dailyWorkouts = ["100 Pushups", "100 Situps", "Run 10 minutes"];
   return (
-    <div className="border-2 border-red-500 h-full items-center">
+    <div className="flex flex-col border-2 border-red-500 h-full items-center justify-evenly">
       <div className="border-2 border-blue-500 text-center text-3xl">
         Daily Goals
       </div>
       {dailyWorkouts.map((item, index) => (
         <div
           key={index}
-          className="border-2 border-blue-500 text-center text-xl"
+          className="border-2 border-blue-500 text-center text-2xl w-1/2"
         >
           {item}
         </div>
       ))}
+      <div className="border-2 border-blue-500 text-center text-2xl w-1/2 align-center">
+        Detailed View
+      </div>
     </div>
   );
 };
